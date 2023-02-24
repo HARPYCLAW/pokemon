633d8870d6425a665fbad001a0084f446e9dd253
HARPYCLAW <harshitvats3007@gmail.com>
2023-02-24 22:49:43 +0530

    Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,15 @@ function Footer(){
     return(
         <div className = 'footer flex justify-center my-3'>
             <div className = 'flex justify-around'>
-                <a href = "https://www.kalashsharma.com/" target = "_blank">
+                <a href = "https://harpyclaw.netlify.app/" target = "_blank">
                     <img className = "mx-2" width = "28px" height = "28px" src = '/web.png' alt = 'website' />
                 </a>
 
-                <a href = "https://github.com/k99sharma" target = "_blank">
+                <a href = "https://github.com/HARPYCLAW" target = "_blank">
                     <img className = "mx-2" width = "28px" height = "28px" src = '/github.png' alt = 'github' />
                 </a>
 
-                <a href = "https://www.linkedin.com/in/kalashsharma99/" target = "_blank">
+                <a href = "https://www.linkedin.com/in/harshitvats07/" target = "_blank">
                     <img className = "mx-2" width = "28px" height = "28px" src = '/linkedin.png' alt = 'linkedin' />
                 </a>
             </div>
